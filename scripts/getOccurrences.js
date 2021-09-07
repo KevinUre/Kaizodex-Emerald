81e12a904cd112187fe70694bf5ba9d9a558e1ec
Kevin Ure <Kevin.M.Ure@gmail.com>
2021-09-07 19:22:46 -0400

    parsed the encounters doc

---
+++ scripts/getOccurrences.js
@@ -0,0 +1,73 @@
+const fs = require('fs')
+const readline = require('readline')
+const path = require('path')
+const { exit } = require('process')
+
+
+
+const data = []
+
+async function process() {
+    var tempLocation = null
+    var tempSubLocation = null
+
+    const reader = readline.createInterface({
+        input: fs.createReadStream(path.join(__dirname,'../input_data/Encounters.txt')),
+        crlfDelay: Infinity
+    })
+
+    for await (const line of reader) {
+        if(tempLocation === null) { // new location
+            if (line.length == 0) {console.log(`Expected new Location`); exit(1) }
+            tempLocation = {
+                Location: line
+            }
+        }
+        else if(line.includes(':')) { // new encounter
+            const parts = line.split(':',2)
+            const pokemon = parts[1].split(',')
+            const encounter = {
+                Type: parts[0],
+                Pokemon: []
+            }
+            for (const poke of pokemon) {
+                const regexpPokemon = /(\w*?)\s*?(\d*?)\%/;
+                if(poke.trim().length == 0) {continue; }
+                const match = poke.match(regexpPokemon);
+                encounter.Pokemon.push({
+                    Pokemon: match[1],
+                    Frequency: match[2]
+                })
+            }
+            if (tempSubLocation === null) {
+                if (!tempLocation.Encounters) { tempLocation.Encounters = [] } //first encounter
+                tempLocation.Encounters.push(encounter)
+            } else {
+                tempSubLocation.Encounters.push(encounter)
+            }
+        }
+        else if (line.length > 0 ) { // Sub-location
+            if (!tempLocation.SubLocations) { tempLocation.SubLocations = [] } //first sub-location
+            if(tempSubLocation !== null) { // already working on a sub-loc
+                tempLocation.SubLocations.push(tempSubLocation)
+                tempSubLocation = null
+            }
+            tempSubLocation = {
+                Name: line,
+                Encounters: []
+            }
+        } else if(line.length == 0) {
+            if(tempSubLocation !== null) { // close open sub-location
+                tempLocation.SubLocations.push(tempSubLocation)
+                tempSubLocation = null
+            }
+            data.push(tempLocation)
+            tempLocation = null
+        }
+        else { console.log(`Unexpected Line: ${line}`); exit(1) }
+    }
+
+    fs.writeFileSync(path.join(__dirname,'../input_data/Occurrences.json'),JSON.stringify(data))
+}
+
+process()
